d9bd75d0c3103c74b39b8f0f5d2debba082ba888
burton-tyll <160846164+burton-tyll@users.noreply.github.com>
2024-06-11 10:23:47 +0200

    ajout du formulaire d'inscription et du style global

---
+++ assets/views/inscription.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/global.css">
+    <link rel="stylesheet" href="../styles/inscription.css">
+    <title>Quiz Night</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <img class="logo" src="../img/logo.png" alt="logo">
+        </nav>
+    </header>
+    <main>
+        <section id="inscription">
+            <div class="section">
+                <form action="inscription.php">
+                    <h1>Inscrivez Vous!</h1>
+                    <input type="text" placeholder="Nom">
+                    <input type="text" placeholder="Prénom">
+                    <input type="mail" placeholder="Adresse e-mail">
+                    <input type="password" placeholder="Mot de passe">
+                    <input type="text" placeholder="Nom d'utilisateur">
+                    <button>M'inscrire</button>
+                </form>
+            </div>
+        </section>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
