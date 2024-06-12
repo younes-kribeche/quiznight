33e7b064a296a10a906149ea694756a86057eaaf
burton-tyll <160846164+burton-tyll@users.noreply.github.com>
2024-06-12 11:20:56 +0200

    securité formulaire d'inscription

--- assets/views/inscription.php
+++ assets/views/inscription.php
@@ -1,16 +1,41 @@
 <?php
 require_once '../../Database.php';
 
+$error_message = null;
+
 $database = new Database();
 
 // Établir la connexion
 $conn = $database->connect();
 
-// Vérifier si la connexion est établie
-if ($conn) {
-    
-} else {
-    echo 'Échec de la connexion.';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $username = $_POST['username'];
+
+    // Vérifier si la connexion est établie
+    if (isset($email, $password, $username) && $conn) {
+        // Vérifier si le mot de passe répond aux exigences
+        if (strlen($password) < 8 || !preg_match("#[0-9]+#", $password) || !preg_match("#[A-Z]+#", $password) || !preg_match("#[a-z]+#", $password) || !preg_match("/[!@#$%^&*()\-_=+]/", $password)) {
+            $error_message = "Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial.";
+        } else {
+            // Hachage du mot de passe
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+            // Préparer la requête d'insertion avec des paramètres de substitution
+            $query = "INSERT INTO user (email, password, name) VALUES (?, ?, ?)";
+            $stmt = $conn->prepare($query);
+
+            // Exécuter la requête en fournissant les valeurs directement
+            $stmt->execute([$email, $hashed_password, $username]);
+
+            // Rediriger l'utilisateur vers une page de confirmation
+            header("Location: inscription.php");
+            exit;
+        }
+    } else {
+        echo 'Échec de la connexion.';
+    }
 }
 
 // Fermer la connexion
@@ -37,14 +62,13 @@ $database->disconnect();
     <main>
         <section id="inscription">
             <div class="section">
-                <form action="inscription.php">
+                <form action="inscription.php" method="POST">
                     <h1>Inscrivez Vous!</h1>
-                    <input type="text" placeholder="Nom">
-                    <input type="text" placeholder="Prénom">
-                    <input type="mail" placeholder="Adresse e-mail">
-                    <input type="password" placeholder="Mot de passe">
-                    <input type="text" placeholder="Nom d'utilisateur">
-                    <button>M'inscrire</button>
+                    <input type="email" placeholder="Adresse e-mail" name="email" required>
+                    <input type="password" placeholder="Mot de passe" name="password" maxlength="30" required>
+                    <?php if($error_message != null){echo ('<p style="color: red; width: 80%">'.$error_message.'</p>');} ?>
+                    <input type="text" placeholder="Nom d'utilisateur" name="username" minlength="6" maxlength="15" required>
+                    <button type="submit">M'inscrire</button>
                 </form>
             </div>
         </section>
